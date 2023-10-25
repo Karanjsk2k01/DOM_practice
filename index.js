e5e89ba78643fad1b1724a311e6e9233b38ff9e6
Karanjsk2k01 <karanvarun14501@gmail.com>
2023-10-25 12:52:41 +0530

    Practised getelementbyclassname

--- index.js
+++ index.js
@@ -15,6 +15,7 @@
 let header = document.getElementById('header-title');
 let mainheader = document.getElementById('main-header');
 let item = document.querySelector('.title');
+let title = document.getElementsByClassName('title');
 // console.log(header.textContent);
 // console.log(header.innerText);
 // console.log(header.innerHTML);
@@ -22,4 +23,20 @@ let item = document.querySelector('.title');
 mainheader.style.borderBottom = 'solid 5px black';
 item.style.fontWeight = 'bold';
 item.style.color = 'green';
-console.log(item)
+// console.log(item)
+// console.log(title);
+
+//getelementbyclassname
+
+let list = document.getElementsByClassName('list-group-item');
+
+// console.log(list);
+// Make the 3 rd element in the list have green background color
+list[2].style.backgroundColor = 'green';
+// Make all the elements in the list have bold color font
+
+for (let i = 0; i < list.length; i++) {
+
+  list[i].style.fontWeight = 'bold';
+
+}
